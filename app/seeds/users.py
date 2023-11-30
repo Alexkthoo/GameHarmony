0209b6c7054fce17171bf7c3b8a45eb861fbbce6
Alex <Alexk.Thoo@gmail.com>
2023-11-29 22:24:45 -0800

    remove faker in seed

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,8 +1,6 @@
 from app.models import db, User, environment, SCHEMA
 from sqlalchemy.sql import text
-from faker import Faker
 
-fake = Faker()
 
 
 # Adds a demo user, you can add other users here if you want
