90a3027a2a0df67cf8c724925edab91031e7977c
Alex Thoo <Alexk.Thoo@gmail.com>
2023-12-11 00:23:21 -0800

    update

--- react-app/src/components/Games/DeleteGame/index.js
+++ react-app/src/components/Games/DeleteGame/index.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { deleteGameThunk } from "../../../store/game";
 
-function DeleteGame({gameId}) {
+function DeleteGame({ gameId }) {
   const { id } = useParams();
   const dispatch = useDispatch();
   const history = useHistory();
@@ -17,7 +17,7 @@ function DeleteGame({gameId}) {
   return (
     <>
       <div id="delete-game">
-        <h2>Are you sure you want to remove this game from the listings?</h2>
+        {/* <h2>Are you sure you want to remove this game from the listings?</h2> */}
         <div id="delete-game-button">
           <button id="top-button-delete" onClick={yesbutton}>
             Yes (Delete Game)
